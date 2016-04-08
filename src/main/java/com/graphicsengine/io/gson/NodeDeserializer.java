67070e8f62830ef2e9ae11d62a55e465d5685a2c
Richard Sahlin <richard_sahlin@yahoo.com>
2016-04-08 19:54:32 +0200

    Node deserializer extends Nucleus deserializer

--- src/main/java/com/graphicsengine/io/gson/NodeDeserializer.java
+++ src/main/java/com/graphicsengine/io/gson/NodeDeserializer.java
@@ -2,7 +2,6 @@ package com.graphicsengine.io.gson;
 
 import java.lang.reflect.Type;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
@@ -15,49 +14,36 @@ import com.graphicsengine.scene.SharedMeshQuad;
 import com.graphicsengine.spritemesh.SpriteMeshNode;
 import com.graphicsengine.ui.Element;
 import com.nucleus.Error;
+import com.nucleus.io.NucleusNodeDeserializer;
 import com.nucleus.scene.Node;
 
-public class NodeDeserializer implements JsonDeserializer<Node> {
-
-    private Gson gson;
-
-    public void setGson(Gson gson) {
-        this.gson = gson;
-    }
+public class NodeDeserializer extends NucleusNodeDeserializer implements JsonDeserializer<Node> {
 
     @Override
     public Node deserialize(JsonElement json, Type type, JsonDeserializationContext context)
             throws JsonParseException {
-        
         JsonObject obj = json.getAsJsonObject();
-        GraphicsEngineNodeType t = GraphicsEngineNodeType.valueOf(obj.get("type").getAsString());
-        switch (t) {
-        case playfieldNode:
-            return gson.fromJson(json, PlayfieldNode.class);
-        case element:
-            return gson.fromJson(json, Element.class);
-        case quadNode:
-            return gson.fromJson(json, QuadParentNode.class);
-        case sharedMeshNode:
-            return gson.fromJson(json, SharedMeshQuad.class);
-        case spriteMeshNode:
-            return gson.fromJson(json, SpriteMeshNode.class);
-        default:
-            throw new IllegalArgumentException(Error.NOT_IMPLEMENTED.message);
+        try {
+            GraphicsEngineNodeType t = GraphicsEngineNodeType.valueOf(obj.get("type").getAsString());
+            switch (t) {
+            case playfieldNode:
+                return gson.fromJson(json, PlayfieldNode.class);
+            case element:
+                return gson.fromJson(json, Element.class);
+            case quadNode:
+                return gson.fromJson(json, QuadParentNode.class);
+            case sharedMeshNode:
+                return gson.fromJson(json, SharedMeshQuad.class);
+            case spriteMeshNode:
+                return gson.fromJson(json, SpriteMeshNode.class);
+            default:
+                throw new IllegalArgumentException(Error.NOT_IMPLEMENTED.message);
+            }
+        } catch (IllegalArgumentException e) {
+            //Try with super
+            return super.deserialize(json, type, context);
         }
         
-        
-        /*
-         * JsonObject obj = json.getAsJsonObject();
-         * com.nucleus.bounds.Bounds.Type t = com.nucleus.bounds.Bounds.Type
-         * .valueOf(obj.get(Bounds.SerializeNames.type.name()).getAsString());
-         * JsonArray array = obj.get(Bounds.SerializeNames.bounds.name()).getAsJsonArray();
-         * float[] values = new float[array.size()];
-         * for (int i = 0; i < array.size(); i++) {
-         * values[i] = array.get(i).getAsFloat();
-         * }
-         * return BoundsFactory.create(t, values);
-         */
     }
 
 }
