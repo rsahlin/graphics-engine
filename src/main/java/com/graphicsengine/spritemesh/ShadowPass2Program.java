7d3a0d17b60fbb91a1ae296d6d3bf5e516370024
Richard Sahlin <richard_sahlin@yahoo.com>
2018-01-07 20:39:27 +0100

    Set shadow render texture parameters in shaderprogram

--- src/main/java/com/graphicsengine/spritemesh/ShadowPass2Program.java
+++ src/main/java/com/graphicsengine/spritemesh/ShadowPass2Program.java
@@ -5,12 +5,19 @@ import com.nucleus.common.Constants;
 import com.nucleus.geometry.Mesh;
 import com.nucleus.io.ExternalReference;
 import com.nucleus.opengl.GLES20Wrapper;
+import com.nucleus.opengl.GLESWrapper.GLES20;
+import com.nucleus.opengl.GLESWrapper.GLES30;
 import com.nucleus.opengl.GLException;
 import com.nucleus.renderer.Pass;
 import com.nucleus.shader.ShaderVariables;
+import com.nucleus.texturing.ParameterData;
 import com.nucleus.texturing.Texture2D;
 import com.nucleus.texturing.TextureFactory;
 import com.nucleus.texturing.TextureParameter;
+import com.nucleus.texturing.TextureParameter.Name;
+import com.nucleus.texturing.TextureParameter.Param;
+import com.nucleus.texturing.TextureParameter.Parameter;
+import com.nucleus.texturing.TextureParameter.Target;
 import com.nucleus.texturing.TextureType;
 import com.nucleus.texturing.TextureUtils;
 import com.nucleus.vecmath.Matrix;
@@ -26,10 +33,15 @@ public class ShadowPass2Program extends TiledSpriteProgram {
 
     public ShadowPass2Program(Pass pass, String category, Texture2D.Shading shading) {
         super(pass, shading, category);
+        // This defines the texture parameters for the shadow pass.
         shadow = TextureFactory.createTexture(TextureType.Texture2D);
         ExternalReference ref = new ExternalReference(ExternalReference.ID_LOOKUP + "DEPTHshadow");
         shadow.setExternalReference(ref);
-        shadow.set(new TextureParameter(TextureParameter.DEFAULT_TEXTURE_PARAMETERS));
+        shadow.set(new TextureParameter(
+                new Parameter[] { Parameter.LINEAR, Parameter.LINEAR, Parameter.CLAMP, Parameter.CLAMP }));
+        ParameterData[] extra = new ParameterData[] {
+                new ParameterData(Target.TEXTURE_2D, Name.TEXTURE_COMPARE_MODE, Param.COMPARE_REF_TO_TEXTURE),
+                new ParameterData(Target.TEXTURE_2D, Name.TEXTURE_COMPARE_FUNC, Param.LESS) };
     }
 
     @Override
@@ -55,6 +67,8 @@ public class ShadowPass2Program extends TiledSpriteProgram {
         }
         int unit = samplers[shaderVariables[ShaderVariables.uShadowTexture.index].getOffset()];
         TextureUtils.prepareTexture(gles, shadow, unit);
+        gles.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES30.GL_TEXTURE_COMPARE_MODE, GLES30.GL_COMPARE_REF_TO_TEXTURE);
+        gles.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES30.GL_TEXTURE_COMPARE_FUNC, GLES20.GL_LESS);
         Texture2D texture = mesh.getTexture(Texture2D.TEXTURE_0);
         if (texture != null && texture.textureType != TextureType.Untextured) {
             TextureUtils.prepareTexture(gles, texture,
