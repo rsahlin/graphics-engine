1ba7f2508021d6b854a7c36a89a635423c28a1c6
Richard Sahlin <richard_sahlin@yahoo.com>
2019-02-05 15:57:08 +0100

    wip, support for buttonpress anim

--- src/main/java/com/graphicsengine/scene/SharedMeshButton.java
+++ src/main/java/com/graphicsengine/scene/SharedMeshButton.java
@@ -10,6 +10,8 @@ public class SharedMeshButton extends SharedMeshQuad implements Button {
     protected static FrameSampler timeKeeper = FrameSampler.getInstance();
 
     protected Action action = Action.NONE;
+    protected float duration;
+    protected float clickAnimTimeout = 0.3f;
 
     public SharedMeshButton() {
         super();
@@ -28,17 +30,28 @@ public class SharedMeshButton extends SharedMeshQuad implements Button {
 
     @Override
     public void clicked() {
+        switch (action) {
+            case NONE:
+            case PRESSED:
+                buttonClicked();
+
+        }
+    }
+
+    private void buttonClicked() {
         action = Action.CLICKED;
         this.getTransform().setScale(new float[] { 0.7f, 0.7f, 0.7f });
         updateTransform();
+        duration = 0;
     }
 
     @Override
     protected void prepareRender() {
         switch (action) {
             case CLICKED:
-                float delta = timeKeeper.getDelta();
+                duration += timeKeeper.getDelta();
                 this.getTransform().setScale(new float[] { 1f, 1f, 1f });
+                updateTransform();
                 action = Action.NONE;
             default:
         }
